a8d9ac8bc1e9d7fc91df1e094e58dc58af299d9f
softprops <d.tangren@gmail.com>
2020-02-09 22:57:21 -0500

    test cov for input

--- __tests__/input.test.ts
+++ __tests__/input.test.ts
@@ -3,7 +3,7 @@ import { parseInput } from "../src/input";
 
 describe("input", () => {
   describe("parseInput", () => {
-    it("parses config from env", () => {
+    it("parses config from env with custom inputs", () => {
       assert.deepEqual(
         parseInput({
           GITHUB_TOKEN: "s3cr3t",
@@ -23,6 +23,27 @@ describe("input", () => {
           runId: 1,
           continueAfterSeconds: 10,
           pollIntervalSeconds: 5
+        }
+      );
+    });
+    it("parses config from env with defaults", () => {
+      assert.deepEqual(
+        parseInput({
+          GITHUB_TOKEN: "s3cr3t",
+          GITHUB_REF: "refs/heads/foo",
+          GITHUB_REPOSITORY: "softprops/turnstyle",
+          GITHUB_WORKFLOW: "test",
+          GITHUB_RUN_ID: "1"
+        }),
+        {
+          githubToken: "s3cr3t",
+          owner: "softprops",
+          repo: "turnstyle",
+          branch: "foo",
+          workflowName: "test",
+          runId: 1,
+          continueAfterSeconds: undefined,
+          pollIntervalSeconds: 60
         }
       );
     });
