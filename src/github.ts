35d47b4c566ee98e1982c7d926c9d740519f35c2
Zachary Whaley <zach.b.whaley@gmail.com>
2024-07-17 15:12:54 -0500

    feat: wait for "waiting" runs as well as "in_progress" and "queued" (#63)
    * Wait for "waiting" runs as well as in_progress and queued
    
    * run code style fix
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Rui Chen <rui@chenrui.dev>

--- src/github.ts
+++ src/github.ts
@@ -62,6 +62,10 @@ export class OctokitGitHub {
       ...options,
       status: "queued" as const,
     };
+    const waiting_options = {
+      ...options,
+      status: "waiting" as const,
+    };
 
     const in_progress_runs = this.octokit.paginate(
       this.octokit.actions.listWorkflowRuns,
@@ -73,8 +77,13 @@ export class OctokitGitHub {
       queued_options,
     );
 
-    return Promise.all([in_progress_runs, queued_runs]).then((values) =>
-      values.flat(),
+    const waiting_runs = this.octokit.paginate(
+      this.octokit.actions.listWorkflowRuns,
+      waiting_options,
+    );
+
+    return Promise.all([in_progress_runs, queued_runs, waiting_runs]).then(
+      (values) => values.flat(),
     );
   };
 }
